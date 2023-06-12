c7bbad6abc3bee69389ed564e8bbdf4aea2e1fa7
Rishabh Agnihotri <rishabh.agnihotri@beamstacks.com>
2023-06-12 16:11:03 +0530

    completed

--- mernapp/src/components/Card.js
+++ mernapp/src/components/Card.js
@@ -1,6 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatchCart, useCart } from "./ContextReducer";
 
-export const Card = () => {
+export const Card = (props) => {
+  let dispatch = useDispatchCart();
+  let data = useCart();
+  const priceRef = useRef();
+  let options = props.options;
+  let priceOptions = Object.keys(options);
+  const [qty, setQty] = useState(1);
+  const [size, setSize] = useState("");
+  // let foodItem = props.item;
+
+  const handleAddToCart = async () => {
+    let food = [];
+    for (const item of data) {
+      // console.log(item.id);
+      // console.log(props.foodItems);
+      
+      if (item.id === props.foodItems._id) {
+        food = item;
+         
+        
+        break;
+      }
+    }
+    console.log(food);
+    console.log(new Date())
+    if(food.length !== 0) {
+      if (food.size === size) {
+        await dispatch({
+          type: "UPDATE",
+          id: props.foodItems._id,
+          price: finalPrice,
+          qty: qty,
+        });
+        console.log(new Date());
+        return;
+      } else if (food.size !== size) {
+        await dispatch({
+          type: "ADD",
+          id: props.foodItems._id,
+          name: props.foodItems.name,
+          price: finalPrice,
+          qty: qty,
+          size: size,
+          img: props.foodItems.img,
+        });
+        console.log("Size different so simply ADD one more to the list");
+        return;
+      }
+      return;
+    }
+    console.log("Hi");
+    
+    await dispatch({
+      type: "ADD",
+      id: props.foodItems._id,
+      name: props.foodItems.name,
+      price: finalPrice,
+      qty: qty,
+      size: size,
+      img: props.foodItems.img,
+    });
+    
+    console.log(data);
+    
+  };
+
+  let finalPrice = qty * parseInt(options[size]);
+
+  useEffect(() => {
+    setSize(priceRef.current.value);
+  }, []);
+
   return (
     <div>
       <div>
@@ -8,12 +80,21 @@ export const Card = () => {
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}
         >
-          <img src="https://www.google.co.in/imgres?imgurl=https%3A%2F%2Fcdn.britannica.com%2F36%2F123536-050-95CB0C6E%2FVariety-fruits-vegetables.jpg&tbnid=rMZ8I5b2kc3W3M&vet=12ahUKEwi1mLHSna7_AhVT2XMBHVxYBI4QMygBegUIARDmAQ..i&imgrefurl=https%3A%2F%2Fwww.britannica.com%2Ftopic%2Ffood&docid=DJp6ra_owrcaaM&w=1600&h=1068&itg=1&q=food&hl=en&ved=2ahUKEwi1mLHSna7_AhVT2XMBHVxYBI4QMygBegUIARDmAQ" className="card-img-top" alt="..." />
+          <img
+            src={props.foodItems.img}
+            className="card-img-top"
+            style={{ maxHeight: "150px", objectFit: "fill" }}
+            alt=""
+          />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build</p>
+            <h5 className="card-title">{props.foodItems.name}</h5>
             <div className="container w-100">
-              <select className="m-2 h-100  bg-success rounded">
+              <select
+                className="m-2 h-100  bg-success rounded"
+                onChange={(e) => {
+                  setQty(e.target.value);
+                }}
+              >
                 {Array.from(Array(6), (e, i) => {
                   return (
                     <option key={i + 1} value={i + 1}>
@@ -23,13 +104,31 @@ export const Card = () => {
                 })}
               </select>
 
-              <select className="m-2 h-100  bg-success rounded">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
+              <select
+                className="m-2 h-100  bg-success rounded"
+                ref={priceRef}
+                onChange={(e) => {
+                  setSize(e.target.value);
+                }}
+              >
+                {priceOptions.map((data) => {
+                  return (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  );
+                })}
               </select>
 
-              <div className="d-inline h-100 fs-5">Total Price</div>
+              <div className="d-inline h-100 fs-5">Rs.{finalPrice}</div>
             </div>
+            <hr />
+            <button
+              className="btn btn-success justify-center ms-2"
+              onClick={handleAddToCart}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -1,39 +1,45 @@
 import React, { useState } from "react";
-import { Link ,useNavigate} from "react-router-dom";
+import { Link,useNavigate} from "react-router-dom";
 
-export const Login = () => {
+export default function Signup() {
   const [credentials, setcredentials] = useState({
+    name: "",
     email: "",
     password: "",
+    geolocation: "",
   });
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("http://localhost:5000/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        name: credentials.name,
         email: credentials.email,
         password: credentials.password,
+        location: credentials.geolocation,
       }),
     });
     const json = await response.json();
     console.log(json);
 
     if (!json.success) {
-      alert("Enter valid credentials");
+      if(json.message){
+        alert(json.message);
+      }else{
+        alert("Enter valid credentials");
+
+      }
     }
 
     if(json.success){
-      localStorage.setItem("userEmail",credentials.email)
-      localStorage.setItem("authToken",json.authToken);
-      console.log(localStorage.getItem("authToken"));
-      navigate("/");
-    }
+        navigate("/login");
+      }
   };
 
   const onChange = (event) => {
@@ -44,6 +50,18 @@ export const Login = () => {
     <>
       <div className="container">
         <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={credentials.name}
+              onChange={onChange}
+            />
+          </div>
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
@@ -74,15 +92,28 @@ export const Login = () => {
               onChange={onChange}
             />
           </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="geolocation"
+              value={credentials.geolocation}
+              onChange={onChange}
+            />
+          </div>
 
           <button type="submit" className="m-3 btn btn-success">
             Submit
           </button>
-          <Link to="/createuser" className="m-3 btn btn-danger">
-            New user?
+          <Link to="/login" className="m-3 btn btn-danger">
+            Already a user
           </Link>
         </form>
       </div>
     </>
   );
-};
+}
